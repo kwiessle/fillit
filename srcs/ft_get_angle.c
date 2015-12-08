6d8b5631cacd07306a64993c189a7cfe6d1b87e8
Victor Quesnel <victor@MacBook-Pro-de-VICTOR-QUESNEL.local>
2015-12-08 20:46:25 +0100

    angle

---
+++ srcs/ft_get_angle.c
@@ -0,0 +1,26 @@
+#include "../includes/fillit.h"
+#include "../includes/libft.h"
+
+int		ft_get_angle(char *map)
+{
+	int i;
+
+	i = 0;
+	while(map[i])
+	{
+		if (map[i] ==  '#')
+		{
+			if (map[i + 1] = '#' && map[i + 6] = '#')
+				return (0);
+			else if (map[i + 4] = '#' && map[i + 5] = '#')
+				return (0);
+			else if (map[i + 5] = '#' && map[i + 6] = '#')
+				return (0);
+			else if (map[i + 1] = '#' && map[i + 5] = '#')
+				return (0):
+		}
+		else
+			i++;
+	}
+	return (1);
+}
