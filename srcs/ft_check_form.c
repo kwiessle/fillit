89fb4df04e4e93d20a6405df34570a6e2757ae60
Victor <victor-quesnel@hotmail.fr>
2015-12-12 14:40:53 +0100

    form

--- srcs/ft_check_form.c
+++ srcs/ft_check_form.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include "../includes/fillit.h"
 
@@ -9,9 +8,11 @@ size_t		ft_is_o(char *map)
 	i = 0;
 	while (map[i])
 	{
-		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 5] == '#' && map[i + 6] == '#'))
+		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 5] == '#' &&\
+					map[i + 6] == '#'))
 			return (0);
-		i++;
+		else
+			i++;
 	}
 	return (1);
 }
@@ -23,132 +24,155 @@ size_t		ft_is_i(char *map)
 	i = 0;
 	while (map[i])
 	{
-		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 2] == '#' && map[i + 3] == '#'))
+		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 2] == '#' &&\
+					map[i + 3] == '#'))
 			return (0);
-		if ((map[i] == '#' && map[i + 5] == '#') && (map[i + 10] == '#' && map[i + 15] == '#'))
+		if ((map[i] == '#' && map[i + 5] == '#') && (map[i + 10] == '#' &&\
+					map[i + 15] == '#'))
 			return (0);
-		i++;
+		else
+			i++;
 	}
 	return (1);
 }
 
 size_t		ft_is_z(char *map)
 {
-		size_t		i;
+	size_t		i;
 
 	i = 0;
 	while (map[i])
 	{
-		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 6] == '#' && map[i + 7] == '#'))
+		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 6] == '#' &&\
+					map[i + 7] == '#'))
 			return (0);
-		if ((map[i] == '#' && map[i + 4] == '#') && (map[i + 5] == '#' && map[i + 9] == '#'))
+		if ((map[i] == '#' && map[i + 4] == '#') && (map[i + 5] == '#' &&\
+					map[i + 9] == '#'))
 			return (0);
-		i++;
+		else
+			i++;
 	}
 	return (1);
 }
 
 size_t		ft_is_s(char *map)
 {
-		size_t		i;
+	size_t		i;
 
 	i = 0;
 	while (map[i])
 	{
-		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 4] == '#' && map[i + 5] == '#'))
+		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 4] == '#' &&\
+					map[i + 5] == '#'))
 			return (0);
-		if ((map[i] == '#' && map[i + 5] == '#') && (map[i + 6] == '#' && map[i + 11] == '#'))
+		if ((map[i] == '#' && map[i + 5] == '#') && (map[i + 6] == '#' &&\
+					map[i + 11] == '#'))
 			return (0);
-		i++;
+		else
+			i++;
 	}
 	return (1);
 }
 
 size_t		ft_is_t(char *map)
 {
-		size_t		i;
+	size_t		i;
 
 	i = 0;
 	while (map[i])
 	{
-		if ((map[i] == '#' && map[i + 5] == '#') && (map[i + 6] == '#' && map[i + 10] == '#'))
+		if (map[i] == '#' && map[i + 5] == '#' && map[i + 6] == '#' &&\
+				map[i + 10] == '#')
 			return (0);
-		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 2] == '#' && map[i + 6] == '#'))
+		else if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 2] == '#' &&\
+					map[i + 6] == '#'))
 			return (0);
-		if ((map[i] == '#' && map[i + 4] == '#') && (map[i + 5] == '#' && map[i + 10] == '#'))
+		else if ((map[i] == '#' && map[i + 4] == '#') && (map[i + 5] == '#' &&\
+					map[i + 10] == '#'))
 			return (0);
-		if ((map[i] == '#' && map[i + 4] == '#') && (map[i + 5] == '#' && map[i + 6] == '#'))
+		else if ((map[i] == '#' && map[i + 4] == '#') && (map[i + 5] == '#' &&\
+					map[i + 6] == '#'))
 			return (0);
-		i++;
+		else
+			i++;
 	}
 	return (1);
 }
 
 size_t		ft_is_l(char *map)
 {
-		size_t		i;
+	size_t		i;
 
 	i = 0;
 	while (map[i])
 	{
-		if ((map[i] == '#' && map[i + 5] == '#') && (map[i + 10] == '#' && map[i + 11] == '#'))
+		if ((map[i] == '#' && map[i + 5] == '#') && (map[i + 10] == '#' &&\
+					map[i + 11] == '#'))
 			return (0);
-		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 2] == '#' && map[i + 5] == '#'))
+		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 2] == '#' &&\
+					map[i + 5] == '#'))
 			return (0);
-		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 6] == '#' && map[i + 11] == '#'))
+		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 6] == '#' &&\
+					map[i + 11] == '#'))
 			return (0);
-		if ((map[i] == '#' && map[i + 3] == '#') && (map[i + 4] == '#' && map[i + 5] == '#'))
+		if ((map[i] == '#' && map[i + 3] == '#') && (map[i + 4] == '#' &&\
+					map[i + 5] == '#'))
 			return (0);
-		i++;
+		else
+			i++;
 	}
 	return (1);
 }
 
 size_t		ft_is_j(char *map)
 {
-		size_t		i;
+	size_t		i;
 
 	i = 0;
 	while (map[i])
 	{
-		if ((map[i] == '#' && map[i + 5] == '#') && (map[i + 9] == '#' && map[i + 10] == '#'))
+		if ((map[i] == '#' && map[i + 5] == '#') && (map[i + 9] == '#' &&\
+					map[i + 10] == '#'))
 			return (0);
-		if ((map[i] == '#' && map[i + 5] == '#') && (map[i + 6] == '#' && map[i + 7] == '#'))
+		if ((map[i] == '#' && map[i + 5] == '#') && (map[i + 6] == '#' &&\
+					map[i + 7] == '#'))
 			return (0);
-		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 5] == '#' && map[i + 10] == '#'))
+		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 5] == '#' &&\
+					map[i + 10] == '#'))
 			return (0);
-		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 2] == '#' && map[i + 7] == '#'))
+		if ((map[i] == '#' && map[i + 1] == '#') && (map[i + 2] == '#' &&\
+					map[i + 7] == '#'))
 			return (0);
-		i++;
+		else
+			i++;
 	}
 	return (1);
 }
 int		main()
 {
-	char *error = "#..#\n....\n....\n#..#\n\0";
+	char *error = "##..\n.##.\n....\n....\n";
 
 	/* T   M A P S*/
-	char	*t1 = "#...\n##..\n#...\n....\n\0";
-	char	*t2 = "....\n....\n.###\n..#.\n\0";
-	char	*t3 = "....\n.#..\n##..\n.#..\n\0";
-	char	*t4 = "....\n.#..\n###.\n....\n\0";
+	char	*t1 = "#...\n##..\n#...\n....\n";
+	char	*t2 = "....\n....\n.###\n..#.\n";
+	char	*t3 = "....\n.#..\n##..\n.#..\n";
+	char	*t4 = "....\n.#..\n###.\n....\n";
 
 	/* L   M A P S*/
-	char	*l1 = "..#.\n..#.\n..##\n....\n\0";
-	char	*l2 = "....\n....\n###.\n#...\n\0";
-	char	*l3 = "....\n.##.\n..#.\n..#.\n\0";
-	char	*l4 = "....\n....\n...#\n.###\n\0";
+	char	*l1 = "..#.\n..#.\n..##\n....\n";
+	char	*l2 = "....\n....\n###.\n#...\n";
+	char	*l3 = "....\n.##.\n..#.\n..#.\n";
+	char	*l4 = "....\n....\n...#\n.###\n";
 
 	/* J    M A P S*/
-	char	*j1 = "....\n...#\n...#\n..##\n\0";
-	char	*j2 = "....\n.#..\n.###\n....\n\0";
-	char	*j3 = "##..\n#...\n#...\n....\n\0";
-	char	*j4 = "....\n....\n###.\n..#.\n\0";
+	char	*j1 = "....\n...#\n...#\n..##\n";
+	char	*j2 = "....\n.#..\n.###\n....\n";
+	char	*j3 = "##..\n#...\n#...\n....\n";
+	char	*j4 = "....\n....\n###.\n..#.\n";
 
 
 	printf("\n\n%s\n\n", error);
 	printf("ERROR = 1   ||   SUCCES = 0");
-	printf("\n\nresult : %zu\n\n", ft_is_j(error));
-
+	printf("\n\nresult : %zu\n\n", ft_is_z(error));
 	return (0);
 }
